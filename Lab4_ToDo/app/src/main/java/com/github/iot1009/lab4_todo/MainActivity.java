c79aa211c94010980ad251cd04d4bc8a315d425a
dongphusang <marcodsang@gmail.com>
2022-03-17 13:48:54 -0400

    Lab04_03_ToDoList
    - Added functions to "confirm" button. Users now able to see inputs after they press the confirm button.

--- Lab4_ToDo/app/src/main/java/com/github/iot1009/lab4_todo/MainActivity.java
+++ Lab4_ToDo/app/src/main/java/com/github/iot1009/lab4_todo/MainActivity.java
@@ -8,15 +8,49 @@ import android.view.View;
 import android.widget.*;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-
+    // declaring components
+    Button confirm;
+    EditText inputField;
+    ListView taskList;
+    ArrayList<String> tasks;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // init components
+        confirm = findViewById(R.id.add_button);
+        inputField = findViewById(R.id.input_field);
+        taskList = findViewById(R.id.task_list);
+
+        tasks = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, tasks);
+        taskList.setAdapter(adapter);
+
+        // assign listeners
+        confirm.setOnClickListener(this);
+
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.add_button)
+            addItem(taskList);
+    }
+
+    // add item to the listview
+    public void addItem(View v) {
+        String input = inputField.getText().toString();
+        if (input.equals(""))
+            inputField.setHint("Enter your task...");
+        else
+            tasks.add(input);
+
+        adapter.notifyDataSetChanged();
+        inputField.setText(""); // set EditText to empty after input
     }
 
 }
